fc0b27c9a87ef39001c8fc0bf190daa4037ec58a
Jason Wu <jasonxhwu@gmail.com>
2024-12-30 17:21:32 -0800

    vim > nano

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@ sudo pacman -R --noconfirm vi
 sudo cp resources/00-brightness-fix.conf /etc/X11/xorg.conf.d/00-brightness-fix.conf
 sudo cp resources/environment /etc/environment
 cp resources/.bashrc ~/.bashrc
+git config --global core.editor "vim"
 yay -S lenovolegionlinux-git vi-vim-symlink 
