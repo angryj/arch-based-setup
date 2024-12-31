cf5d0d69b29bef021236f6cb3402388e4db85443
Jason Wu <jasonxhwu@gmail.com>
2024-12-30 17:20:24 -0800

    config files

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 sudo pacman -Syy --noconfirm neofetch htop vim ripgrep fd 
-cp resources/00-brightness-fix.conf /etc/X11/xorg.conf.d/
+sudo pacman -R --noconfirm vi
+sudo cp resources/00-brightness-fix.conf /etc/X11/xorg.conf.d/00-brightness-fix.conf
+sudo cp resources/environment /etc/environment
+cp resources/.bashrc ~/.bashrc
 yay -S lenovolegionlinux-git vi-vim-symlink 
